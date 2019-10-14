d87d5637f77db91bc4afb2acc222c262add87a71
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-10-14 14:45:10 -0700

    created CPU class and RAM functions

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
-"""Main."""
+class CPU:
+    def __init__(self):
+        self.register = [0] * 8
+        self.memory = [0] * 256
+        self.pc = 0
+
+    def ram_read(self, MAR): # MAR = Memory Address Register
+        return self.memory[MAR]
+    
+    def raw_write(self, MAR, MDR): #MDR = Memory Data Register
+        self.memory[MAR] = MDR
+
+
 
 import sys
 from cpu import *
