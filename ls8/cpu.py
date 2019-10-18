be1e87cdcd49f45b33ab9a4810cc7fefca8bb79e
Sarahlee671 <sarah.lee1987@hotmail.com>
2019-10-18 00:44:41 -0700

    day 4 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,14 +2,14 @@
 
 import sys
 
+SP = 7
 class CPU:
     def __init__(self):
         self.reg = [0] * 8
         self.ram = [0] * 256
         self.pc = 0
         #R7 is reserved as the SP
-        self.SP = 7
-
+        self.reg[7] = 0xF4
 
     def ram_read(self, MAR): # MAR = Memory Address Register
         return self.ram[MAR]
@@ -22,35 +22,19 @@ class CPU:
 
         try:
             address = 0
-            with open(sys.argv[1]) as file: 
-                for line in file:
-                    # ignore everything after #
-                    split_hashes = line.split("#")
-                    # convert binary strings to integer values to store in RAM 
-                    num = split_hashes[0].strip()
-                    # (can use build-in int() function by specifying a number base as the second argument)
-                    self.ram[address] = int(num, 2)
-                    address += 1
+            with open(sys.argv[1]) as file:
+                #ignore everything after # and space
+                lines = [line for line in file if not (line[0]=='#' or line[0]=='\n')]
+                program = [int(line.split('#')[0].strip(), 2) for line in lines]
+
+            for instruction in program:
+                self.ram[address] = instruction
+                address += 1
+
 
         except FileNotFoundError:
             print(f"{sys.argv[0]}: {sys.argv[1]} not found")
             sys.exit(2)
-
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -85,6 +69,7 @@ class CPU:
         print()
 
     def run(self):
+       
         """Run the CPU."""
         LDI = 0b10000010
         PRN = 0b01000111
@@ -92,6 +77,9 @@ class CPU:
         MUL = 0b10100010
         PUSH = 0b01000101
         POP = 0b01000110
+        CALL = 0b01010000
+        RET = 0b00010001
+        ADD = 0b10100000
 
         running = True
 
@@ -118,23 +106,43 @@ class CPU:
                 print(self.reg[operand_a])
                 self.pc += 2
 
+            elif IR == ADD:
+                self.alu("ADD", operand_a, operand_b)
+                self.pc += 3
+
             elif IR == MUL:
                 self.alu("MUL", operand_a, operand_b)
                 self.pc += 3
 
             elif IR == PUSH:
                 #Decrement the SP
-                self.reg[self.SP] -= 1
+                self.reg[SP] -= 1
                 #Copy the value in the given register to the address pointed to by SP
-                self.ram[self.reg[self.SP]] = self.reg[operand_a]
+                self.ram[self.reg[SP]] = self.reg[operand_a]
                 self.pc += 2
 
             elif IR == POP:
                 #copy the value from the address pointed to by SP to the given register
-                self.reg[operand_a] = self.ram[self.reg[self.SP]]
+                self.reg[operand_a] = self.ram[self.reg[SP]]
                 #Increment SP
-                self.reg[self.SP] += 1
+                self.reg[SP] += 1
                 self.pc += 2
+
+            elif IR == CALL:
+                # Push the return address on the stack
+                self.reg[SP] -= 1
+                self.ram[self.reg[SP]] = self.pc + 2
+                # The PC is set to the address stored in the given register.
+                # We jump to that location in RAM and execute the first instruction in the subroutine
+                reg_num = self.ram[self.pc + 1]
+                self.pc = self.reg[reg_num]
+            
+            elif IR == RET:
+                #Return from subroutine
+                self.pc = self.ram[self.reg[SP]]
+                #Pop the value from the top of the stack and store it in the PC
+                self.reg[SP] += 1
+
             else:
                 print(f"Unknown instruction: {IR}")
                 sys.exit(1)
